d656693e5ee2945fdaaedfab2dd39c0549523fec
Sergio Revilla <sergio.revilla@hey.com>
2022-04-22 13:55:41 +0200

    Add next function to error middleware

--- src/errorMiddleware.ts
+++ src/errorMiddleware.ts
@@ -1,9 +1,10 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response} from 'express';
 import logger from 'src/logger';
 
-const errorMiddleware = async (error: Error, req: Request, res: Response) => {
+const errorMiddleware = (error: Error, req: Request, res: Response, next: NextFunction) => {
   logger.error(error.message, { stacktrace: error.stack });
   res.status(500).json({ error: error.message });
+  next();
 };
 
 export { errorMiddleware };
