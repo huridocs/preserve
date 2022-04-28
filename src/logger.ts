a90c1dded5f649ce6f36f6ce0716302bcb3e66e2
Sergio Revilla <sergio.revilla@hey.com>
2022-04-28 16:55:14 +0200

    Add logger Graylog transport

--- src/logger.ts
+++ src/logger.ts
@@ -1,9 +1,45 @@
-import { createLogger, transports } from 'winston';
+import { createLogger, format, Logger, transports } from 'winston';
+import WinstonGraylog2 from 'winston-graylog2';
+import * as os from 'os';
+import { config } from './config';
 
 const logConfiguration = {
   transports: [new transports.Console()],
 };
 
-const logger = createLogger(logConfiguration);
+const productionConfiguration = {
+  // format: format.combine(format.errors({ stack: true }), format.timestamp(), format.json()),
+  format: format.combine(format.timestamp(), format.json()),
+  expressFormat: true,
+  defaultMeta: {
+    service: 'admin-service',
+  },
+  transports: [
+    new WinstonGraylog2({
+      name: 'Graylog',
+      level: 'info',
+      silent: false,
+      handleExceptions: true,
+      exceptionsLevel: 'error',
+      graylog: {
+        servers: [{ host: 'graylog.huridata.org', port: 12201 }],
+        hostname: os.hostname(),
+        facility: 'preserve-api',
+      },
+      // staticMeta: { env: config.ENVIRONMENT },
+    }),
+    new transports.Console(),
+  ],
+};
+
+const loggerFactory = (): Logger => {
+  if (config.ENVIRONMENT === 'development') {
+    return createLogger(logConfiguration);
+  }
+
+  return createLogger(productionConfiguration);
+};
+
+const logger = loggerFactory();
 
 export { logger };
