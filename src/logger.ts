ce6199b211aaa920f360c4c221cae074d8b6880c
Sergio Revilla <sergio.revilla@hey.com>
2022-05-06 15:18:32 +0200

    Graylog's winston transport for production environments (#88)
    * Added dependencies
    
    * Add logger Graylog transport
    
    * Add winston transport for Graylog
    
    * Use logger instead of stdout
    
    * Fix types
    
    * Remove eslint rule

--- src/logger.ts
+++ src/logger.ts
@@ -1,9 +1,42 @@
-import { createLogger, transports } from 'winston';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const winston = require('winston');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const WinstonGraylog2 = require('winston-graylog2');
+import * as os from 'os';
+import { config } from './config';
 
 const logConfiguration = {
-  transports: [new transports.Console()],
+  transports: [new winston.transports.Console()],
 };
 
-const logger = createLogger(logConfiguration);
+const productionConfiguration = {
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console(),
+    new WinstonGraylog2({
+      name: 'Graylog',
+      level: 'info',
+      silent: false,
+      handleExceptions: true,
+      exceptionsLevel: 'error',
+      graylog: {
+        servers: [{ host: config.logger.host, port: config.logger.port }],
+        hostname: os.hostname(),
+        facility: 'preserve',
+      },
+      staticMeta: { env: config.ENVIRONMENT },
+    }),
+  ],
+};
+
+const loggerFactory = () => {
+  if (config.ENVIRONMENT === 'development') {
+    return winston.createLogger(logConfiguration);
+  }
+
+  return winston.createLogger(productionConfiguration);
+};
+
+const logger = loggerFactory();
 
 export { logger };
