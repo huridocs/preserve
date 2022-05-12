d2cadf889124ca03508ebc79c1ae9f68f8221486
Sergio Revilla <sergio.revilla@hey.com>
2022-05-12 13:10:43 +0200

    Add Sentry integration for worker (#118)
    * Add Sentry integration for worker
    
    * Add Docker cache
    
    * Add missing step
    
    * Remove Docker cache
    
    * Change compose env variable names
    
    * Better logs
    
    * Remove action step
    
    * Add badges

--- src/worker.ts
+++ src/worker.ts
@@ -1,9 +1,11 @@
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
 import { connectDB, disconnectDB } from './DB';
-
 import { startJobs, stopJobs } from './QueueProcessor';
 import { Vault } from './Vault';
 import { microlinkJob } from './microlinkJob';
 import { logger } from './logger';
+import { config } from './config';
 
 const uncaughtError = (error: unknown) => {
   throw error;
@@ -12,6 +14,19 @@ process.on('unhandledRejection', uncaughtError);
 process.on('uncaughtException', uncaughtError);
 
 connectDB().then(db => {
+  if (config.sentry.worker_dsn) {
+    Sentry.init({
+      release: config.VERSION,
+      dsn: config.sentry.worker_dsn,
+      environment: config.ENVIRONMENT,
+      integrations: [
+        new Tracing.Integrations.Mongo({
+          useMongoose: false,
+        }),
+      ],
+      tracesSampleRate: config.sentry.tracesSampleRate,
+    });
+  }
   startJobs(microlinkJob(logger), new Vault(db), 1000);
   logger.info(`Preserve jobs started`);
 
@@ -19,9 +34,9 @@ connectDB().then(db => {
     logger.info('SIGTERM signal received');
     stopJobs().then(() => {
       disconnectDB().then(() => {
-        logger.info('Disconnected from database');
+        logger.info('Preserve worker disconnected from database');
       });
-      logger.info('Worker stopped successfully');
+      logger.info('Preserve worker stopped successfully');
       process.exit(0);
     });
   });
