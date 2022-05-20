8112b897ca83bd5d3ac3b0921c8738ae6d7686e2
Daneryl <daneryl@gmail.com>
2022-05-20 09:15:54 +0200

    move download folder inside files folder

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,14 @@
 import { version } from '../package.json';
 
 export const config = {
-  evidences_return_max_limit: 50,
   freetsa: {
     pemFile: `${__dirname}/../files/freetsa/cacert.pem`,
     crtFile: `${__dirname}/../files/freetsa/tsa.crt`,
   },
-  mongodb_uri: process.env.MONGO_URI || 'mongodb://localhost:27019',
-  data_path: `${__dirname}/../downloads/`,
+  data_path: `${__dirname}/../files/downloads/`,
   trusted_timestamps_path: `${__dirname}/../files/trusted_timestamps/`,
+  evidences_return_max_limit: 50,
+  mongodb_uri: process.env.MONGO_URI || 'mongodb://localhost:27019',
   sentry: {
     api_dsn: process.env.SENTRY_PRESERVE_API_DSN,
     worker_dsn: process.env.SENTRY_PRESERVE_WORKER_DSN,
