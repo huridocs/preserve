2f1489a92cc7d0a6c96a1e4d8846f17e4058ab98
Sergio Revilla <sergio.revilla@hey.com>
2022-05-25 17:43:26 +0200

    Change data path on API tests

--- specs/Api.spec.ts
+++ specs/Api.spec.ts
@@ -69,7 +69,7 @@ describe('Preserve API', () => {
   }
 
   beforeAll(async () => {
-    config.data_path = `${__dirname}/downloads`;
+    config.data_path = `${__dirname}/files/downloads`;
     config.trusted_timestamps_path = `${__dirname}/trusted_timestamps`;
     db = await connectDB('preserve-api-testing');
     vault = new Vault(db);
