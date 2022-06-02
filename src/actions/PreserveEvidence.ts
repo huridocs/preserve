a3f519395939a1940612d4f4becffc8f0c070881
Sergio Revilla <sergio.revilla@hey.com>
2022-06-02 19:17:04 +0200

    First Evidence method

--- src/actions/PreserveEvidence.ts
+++ src/actions/PreserveEvidence.ts
@@ -3,9 +3,8 @@ import path from 'path';
 // eslint-disable-next-line
 // @ts-ignore
 import fullPageScreenshot from 'puppeteer-full-page-screenshot';
-import { EvidenceDB, PreservationOptions, PreservationResults, VideoDownloader } from 'src/types';
+import { EvidenceDB, FetchClient, PreservationOptions, PreservationResults, VideoDownloader } from 'src/types';
 import { config } from '../config';
-import { FetchClient } from 'src/types';
 import { Browser } from '../infrastructure/Browser';
 
 export class PreserveEvidence {
@@ -23,8 +22,8 @@ export class PreserveEvidence {
     evidence: EvidenceDB,
     options: PreservationOptions = { stepTimeout: 2000 }
   ): Promise<PreservationResults> {
-    const evidence_dir = path.join(config.data_path, evidence._id.toString());
-    await mkdir(evidence_dir);
+    const domainEvidence = new Evidence(evidence);
+    await mkdir(domainEvidence.directory());
     const cookie = evidence.cookies.map(cookie => `${cookie.name}=${cookie.value}`).join(';');
     const response = await this.httpClient.fetch(evidence.attributes.url, {
       headers: { cookie },
@@ -33,7 +32,7 @@ export class PreserveEvidence {
     if (contentType.includes('application/pdf')) {
       const content_path = path.join(evidence._id.toString(), 'content.pdf');
       const array = new Uint8Array(await response.arrayBuffer());
-      await appendFile(path.join(evidence_dir, 'content.pdf'), array);
+      await appendFile(path.join(domainEvidence.directory(), 'content.pdf'), array);
       const fileName = evidence.attributes.url.split('/').pop();
       return new Promise(resolve => {
         const result: PreservationResults = {
@@ -46,7 +45,7 @@ export class PreserveEvidence {
     await this.browser.init();
 
     const html_content_path = path.join(evidence._id.toString(), 'content.html');
-    await appendFile(path.join(evidence_dir, 'content.html'), await response.text());
+    await appendFile(path.join(domainEvidence.directory(), 'content.html'), await response.text());
 
     return new Promise(async (resolve, reject) => {
       this.browser.page.on('error', (e: Error) => {
@@ -63,7 +62,7 @@ export class PreserveEvidence {
         await this.browser.removeAllStickyAndFixedElements();
         await this.browser.page.waitForTimeout(options.stepTimeout);
         await this.browser.page.screenshot({
-          path: path.join(evidence_dir, 'screenshot.jpg'),
+          path: path.join(domainEvidence.directory(), 'screenshot.jpg'),
         });
         await this.browser.scrollDown(600);
         await this.browser.page.waitForTimeout(options.stepTimeout);
@@ -72,18 +71,18 @@ export class PreserveEvidence {
         await this.browser.removeAllStickyAndFixedElements();
         await this.browser.page.waitForTimeout(options.stepTimeout);
         await fullPageScreenshot(this.browser.page, {
-          path: path.join(evidence_dir, 'full_screenshot.jpg'),
+          path: path.join(domainEvidence.directory(), 'full_screenshot.jpg'),
         });
 
         const text = await this.browser.page.evaluate(() => document.body.innerText);
         const content_path = path.join(evidence._id.toString(), 'content.txt');
-        await appendFile(path.join(evidence_dir, 'content.txt'), text);
+        await appendFile(path.join(domainEvidence.directory(), 'content.txt'), text);
         const title = (await this.browser.page.title()) || evidence.attributes.url;
 
         await this.browser.close();
 
         const video_path = await this.videoDownloader.download(evidence, {
-          output: path.join(evidence_dir, 'video.mp4'),
+          output: path.join(domainEvidence.directory(), 'video.mp4'),
           format: 'best',
           addHeader: `Cookie:${cookie}`,
           noPlaylist: true,
@@ -106,5 +105,16 @@ export class PreserveEvidence {
         reject(e);
       }
     });
+  }
+}
+
+class Evidence {
+  private evidence: EvidenceDB;
+  constructor(evidence: EvidenceDB) {
+    this.evidence = evidence;
+  }
+
+  directory() {
+    return path.join(config.data_path, this.evidence._id.toString());
   }
 }
