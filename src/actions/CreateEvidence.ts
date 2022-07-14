4a86a9ec9e5f813aab833d5607ae612c2736090f
Sergio Revilla <sergio.revilla@hey.com>
2022-07-14 11:21:51 +0200

    Add header to cookie jar file

--- src/actions/CreateEvidence.ts
+++ src/actions/CreateEvidence.ts
@@ -1,8 +1,9 @@
 import { appendFile } from 'fs/promises';
+import { WithId } from 'mongodb';
 import path from 'path';
 import { Logger } from 'winston';
 import { Vault } from 'src/infrastructure/Vault';
-import { User } from 'src/types';
+import { EvidenceDB, User } from 'src/types';
 import { Cookie } from 'src/types';
 import { config } from '../config';
 
@@ -17,28 +18,38 @@ export class CreateEvidence {
 
   async execute(url: string, user: User, cookies: Cookie[] | undefined) {
     const evidence = await this.vault.create(url, user, cookies || []);
-    const netscapeCookies = cookies
-      ?.map(cookie => {
-        const { domain, path, secure, expirationDate, name, value } = cookie;
-
-        return [
-          domain,
-          domain?.startsWith('.') ? 'TRUE' : 'FALSE',
-          path,
-          secure?.toString().toUpperCase(),
-          expirationDate,
-          name,
-          value,
-        ].join('\t');
-      })
-      .join('\n');
-
-    await appendFile(
-      path.join(config.cookiesPath, `${evidence._id.toString()}.txt`),
-      netscapeCookies || ''
-    );
+    await CreateEvidence.createCookieJar(cookies, evidence);
     this.logger.info(`Evidence created for url ${url}`);
 
     return evidence;
+  }
+
+  private static async createCookieJar(
+    cookies: Cookie[] | undefined,
+    evidence: WithId<EvidenceDB>
+  ) {
+    const cookiesHeader = '# Netscape HTTP Cookie File\n';
+    const netscapeCookies =
+      cookiesHeader +
+      cookies
+        ?.map(cookie => {
+          const { domain, path, secure, expirationDate, name, value } = cookie;
+
+          return [
+            domain,
+            domain?.startsWith('.') ? 'TRUE' : 'FALSE',
+            path,
+            secure?.toString().toUpperCase(),
+            expirationDate,
+            name,
+            value,
+          ].join('\t');
+        })
+        .join('\n');
+
+    await appendFile(
+      path.join(config.cookiesPath, `${evidence._id.toString()}.txt`),
+      netscapeCookies
+    );
   }
 }
