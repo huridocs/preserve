1f554a5aec76e69615c57577ce5143bb7f5f324b
rafapolit <rafaelpolit@gmail.com>
2023-07-25 18:15:43 -0500

    Removed entry points

--- worker-entrypoint.sh
+++ worker-entrypoint.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-# USERID=${USER_ID:-9001}
-# GROUPID=${GROUP_ID:-9001}
-# echo "Starting with UID: $USERID, GID: $GROUPID"
-# useradd -u "$USERID" -o -m user
-# groupmod -g "$GROUPID" user
-# chown user:user /home/user/app
-# export HOME=/home/user
-
 if [ "$NODE_ENV" = "production" ]; then
-  # exec /usr/sbin/gosu user node src/worker.js
   exec node src/worker.js
 else
-  # exec /usr/sbin/gosu user yarn worker
   exec yarn worker
 fi
